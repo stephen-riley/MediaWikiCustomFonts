4ec7ef9f40da9495821487a52272a9fd2eb211a4
Stephen Riley <jstephen.riley@yahoo.com>
2026-05-27 22:15:33 -0700

    add alias to special page

---
+++ i18n/MediaWikiCustomFonts.i18n.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for MediaWikiCustomFonts extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English */
+$specialPageAliases['en'] = [
+	'CustomFonts' => [ 'CustomFonts', 'Custom Fonts' ],
+];
